3bca77fc5992c1710198a727af626c533217f4c9
ar125arr13 <ar125arr13@gmail.com>
2026-09-22 12:11:53 +0330

    q.js

--- api/q.js
+++ api/q.js
@@ -51,7 +51,7 @@ export default async function handler(req, res) {
 
     try {
       saveResponse = await fetch(
-        "https://c934de107f389f.lhr.life/save-code",
+        "https://70fa6782c3cbec.lhr.life",
         {
           method: "POST",
           headers: {
